409679e571c7ec8e1c4fbc9051afc61adc5efb3f
bkij <duelbart5@gmail.com>
2017-01-16 16:14:24 +0100

    Add client info creation

--- createMockData.py
+++ createMockData.py
@@ -1,1 +1,15 @@
 import elizabeth as el
+import random
+
+personal = el.Personal('pl')
+business = el.Business('pl')
+datetime = el.Datetime('pl')
+text = el.Text('pl')
+
+with open('clientData.csv', 'wb') as clientOut:
+	# The rows are: firstname, lastname, initial
+	clientWriter = csv.writer(clientOut, delimiter=',')
+	for i in range(0, 10800):
+		randGender = random.choice(['male', 'female'])
+		randInitial = random.choice(text.alphabet(letter_case='upper'))
+		clientWriter.writerow(personal.name(gender = randGender), personal.lastname, randInitial)
