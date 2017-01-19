15221f894d8e81d4522ac694e0eeb0de18659e13
bkij <duelbart5@gmail.com>
2017-01-19 20:32:53 +0100

    Add inserting generated data to the database

---
+++ insertMockData.sql
@@ -0,0 +1,113 @@
+USE Conferences
+
+SET DATEFORMAT dmy
+
+BULK INSERT Clients 
+FROM 'C:\Users\kveld\Desktop\Conferences\clientData.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT Companies
+FROM 'C:\Users\kveld\Desktop\Conferences\companyData.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT Conferences
+FROM 'C:\Users\kveld\Desktop\Conferences\conferenceData.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT ConferenceDays
+FROM 'C:\Users\kveld\Desktop\Conferences\conferenceDaysData.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT ConferenceAttendees
+FROM 'C:\Users\kveld\Desktop\Conferences\conferenceAttendees.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT WorkshopAttendees
+FROM 'C:\Users\kveld\Desktop\Conferences\workshopAttendees.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT Workshops
+FROM 'C:\Users\kveld\Desktop\Conferences\workshops.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT ConferenceReservations
+FROM 'C:\Users\kveld\Desktop\Conferences\conferenceReservations.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT WorkshopReservations
+FROM 'C:\Users\kveld\Desktop\Conferences\workshopReservations.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT ReservationDetails
+FROM 'C:\Users\kveld\Desktop\Conferences\reservationDetails.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
+
+BULK INSERT Payments
+FROM 'C:\Users\kveld\Desktop\Conferences\payments.csv'
+WITH (
+	FIELDTERMINATOR = '~',
+	KEEPIDENTITY,
+	KEEPNULLS,
+	ROWTERMINATOR = '\r\n',
+	DATAFILETYPE = 'widechar'
+)
