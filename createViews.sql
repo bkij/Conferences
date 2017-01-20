9ecd35fc70cfa75564c0acc8884d163995deb8e3
madlen96 <magdalenka.k96@gmail.com>
2017-01-20 19:18:51 +0100

    Views updated

--- createViews.sql
+++ createViews.sql
@@ -1,5 +1,8 @@
--- WIDOK: NAJPOPULARNIEJSZE KONFERENCJE - pokazuje listê 10 konferencji najbardziej obleganych
+USE CONFERENCES
 
+
+-- WIDOK: NAJPOPULARNIEJSZE KONFERENCJE - pokazuje listê 10 konferencji najbardziej obleganych
+GO
 CREATE VIEW [The most popular conferences] AS
 SELECT  TOP 10 SUM(dbo.reservationdetails.num_spots) as [The number of takers], dbo.Conferences.conference_id,  dbo.Conferences.title,
 	dbo.Conferences.date_start, dbo.Conferences.date_end
@@ -16,6 +19,7 @@ order by [The number of takers] desc
 
 
 -- WIDOK: NAJPOPULARNIEJSZE WARSZTATY - pokazuje listê 10 warsztatów najbardziej obleganych
+GO
 CREATE VIEW [The most popular workshops] AS
 SELECT TOP 10 SUM(dbo.reservationdetails.num_spots) as [The number of takers], dbo.Workshops.workshop_id, 
 	dbo.Workshops.conference_day_id, dbo.Workshops.title, dbo.Workshops.num_spots, dbo.Workshops.date, dbo.Workshops.price
@@ -31,6 +35,7 @@ order by [The number of takers] desc
 
 
 -- WIDOK: POKA¯ ANULOWANE REZERWACJE 
+GO
 CREATE VIEW [Cancelled Reservations] AS
 SELECT dbo.ConferenceReservations.reservation_id, dbo.ConferenceReservations.conference_id, 'CONFERENCE' as [Conference/Workshop]
 FROM dbo.ConferenceReservations
@@ -46,3 +51,62 @@ FROM dbo.WorkshopReservations
 
 
 
+
+-- WIDOK (JAKO PROCEDURA): Listy osobowe uczestników konferencji na ka¿dy dzieñ
+GO
+CREATE PROCEDURE CONFERENCE_ATTENDEES_PER_DAY (@ConferenceDayId int) 
+AS
+SELECT dbo.Clients.client_id, dbo.Clients.company_id, dbo.Clients.firstname, dbo.Clients.lastname, dbo.Clients.initial
+from dbo.Clients
+	inner join dbo.ConferenceAttendees
+		on dbo.Clients.client_id = dbo.ConferenceAttendees.client_id
+	inner join dbo.ConferenceDays
+		on dbo.ConferenceDays.conference_day_id = dbo.ConferenceAttendees.conference_day_id 
+where dbo.ConferenceDays.conference_day_id =  @ConferenceDayId
+
+-- WIDOK (jako procedura): Listy osobowe uczestników warsztatu na ka¿dy dzieñ
+GO
+CREATE PROCEDURE WORKSHOP_ATTENDEES_PER_DAY (@ConferenceDayId int)
+AS
+SELECT dbo.Clients.client_id, dbo.Clients.company_id, dbo.Clients.firstname, dbo.Clients.lastname, dbo.Clients.initial
+	from dbo.Clients 
+		inner join dbo.WorkshopAttendees
+			on dbo.Clients.client_id = dbo.WorkshopAttendees.client_id
+        inner join  dbo.Workshops
+			on dbo.Workshops.workshop_id = dbo.WorkshopAttendees.workshop_id
+		inner join dbo.ConferenceDays
+			on dbo.ConferenceDays.conference_day_id = dbo.Workshops.conference_day_id
+where dbo.ConferenceDays.conference_day_id =  @ConferenceDayId
+
+
+-- WIDOK (jako procedura): Listy p³atnoœci per klient 
+GO
+CREATE PROCEDURE PAYMENTS_LIST_PER_CLIENT (@client_id int)
+AS
+SELECT * from dbo.Payments
+	inner join dbo.ReservationDetails
+		on dbo.ReservationDetails.payment_id = dbo.Payments.payment_id
+	inner join dbo.Clients
+		on dbo.ReservationDetails.client_id = dbo.Clients.client_id
+where dbo.Clients.client_id = @client_id
+
+
+-- WIDOK (jako procedura): Listy p³atnoœæ per firma
+     
+
+GO
+CREATE PROCEDURE PAYMENTS_LIST_PER_COMPANY (@company_id int)
+AS
+SELECT * from dbo.Payments
+	inner join dbo.ReservationDetails
+		on dbo.ReservationDetails.payment_id = dbo.Payments.payment_id
+	inner join dbo.Companies
+		on dbo.ReservationDetails.client_id = dbo.Companies.company_id
+where dbo.Companies.company_id = @company_id
+
+
+-- WIDOK : Lista najczêœciej korzystaj¹cych z us³ug firm
+GO 
+CREATE 
+
+-- WIDOK : Lista najczêœciej korzystaj¹cych z us³ug klientów
