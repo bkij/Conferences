efbc847284d6fdb3d8f02ae1a6246328a839a0ae
bkij <duelbart5@gmail.com>
2017-01-22 13:26:53 +0100

    Add best practice check if exists

--- createPermissions.sql
+++ createPermissions.sql
@@ -1,9 +1,13 @@
-﻿CREATE LOGIN sampleLogin WITH PASSWORD = 'samplePassword',
+﻿USE master
+GO
+
+IF EXISTS(SELECT * FROM master..syslogins WHERE name = 'sampleLogin')
+DROP LOGIN sampleLogin
+CREATE LOGIN sampleLogin WITH PASSWORD = 'samplePassword',
 DEFAULT_DATABASE = Conferences,
 DEFAULT_LANGUAGE = Polish,
 CHECK_EXPIRATION = OFF,
-CHECK_POLICY = OFF
-GO;
+CHECK_POLICY = OFF;
 
 -- Programista aplikacji wewnętrznych, do użytku
 -- głównie przez konsultantów telefonicznych
