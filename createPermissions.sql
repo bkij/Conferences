c58d7923c474f33f132cd370776fac88e2a5d15a
bkij <duelbart5@gmail.com>
2017-01-22 12:40:48 +0100

    Add skeleton permissions code

---
+++ createPermissions.sql
@@ -0,0 +1,14 @@
+﻿CREATE LOGIN sampleLogin WITH PASSWORD = 'samplePassword',
+DEFAULT_DATABASE = Conferences,
+DEFAULT_LANGUAGE = Polish,
+CHECK_EXPIRATION = OFF,
+CHECK_POLICY = OFF
+GO;
+
+-- Programista aplikacji wewnętrznych, do użytku
+-- głównie przez konsultantów telefonicznych
+CREATE USER innerDeveloper FOR LOGIN sampleLogin;
+
+-- Programista aplikacji zewnętrznych (webowych, mobilnych)
+-- do korzystania przez klientów
+CREATE USER outerDeveloper FOR LOGIN sampleLogin;
