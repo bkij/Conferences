55524319e4f8c5c1479540e4ea64f3a5c7468e61
bkij <duelbart5@gmail.com>
2017-01-22 13:47:46 +0100

    Add permissions for app devs

--- createPermissions.sql
+++ createPermissions.sql
@@ -10,9 +10,25 @@ CHECK_EXPIRATION = OFF,
 CHECK_POLICY = OFF;
 
 -- Programista aplikacji wewnętrznych, do użytku
--- głównie przez konsultantów telefonicznych
+-- przez organizatorów konferencji (zarządzanie konferencjami i analiza danych)
 CREATE USER innerDeveloper FOR LOGIN sampleLogin;
 
--- Programista aplikacji zewnętrznych (webowych, mobilnych)
--- do korzystania przez klientów
-CREATE USER outerDeveloper FOR LOGIN sampleLogin;
+
+-- Programista aplikacji do zewnętrznych i wewnętrznych
+-- do użytku klientów (aplikacje webowe, mobilne) oraz konsulatntów
+-- firmy (aplikacje wewnętrzne)
+CREATE USER appDeveloper FOR LOGIN sampleLogin;
+
+-- Procdeury
+GRANT EXEC ON Conferences.CREATE_RESERVATION TO appDeveloper;
+GRANT EXEC ON Conferences.NUM_SPOTS_RESERVATION_CHANGE TO appDeveloper;
+GRANT EXEC ON Conferences.RESERVATION_CANCELLATION_CR TO appDeveloper;
+GRANT EXEC ON Conferences.RESERVATION_CANCELLATION_WS TO appDeveloper;
+GRANT EXEC ON Conferences.ADD_RESERVATION_LIST TO appDeveloper;
+
+-- Widoki
+GRANT SELECT ON Conferences.PAYMENTS_HISTORY TO appDeveloper;
+
+-- Funkcje
+GRANT EXEC ON Conferences.FREE_SPOTS_FOR_WS TO appDeveloper;
+GRANT EXEC ON Conferences.FREE_SPOTS_FOR_CONFDAY TO appDeveloper;
