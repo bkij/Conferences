e866f8ca51f4416da7ce34cf7efb5d695deab38f
bkij <duelbart5@gmail.com>
2017-01-23 03:22:32 +0100

    Final fix of job adding

--- addJobs.sql
+++ addJobs.sql
@@ -1,6 +1,9 @@
 -- Okresowo wykonywane procedury
 USE msdb;
 GO
+IF EXISTS(SELECT * FROM msdb.dbo.sysjobs WHERE name = N'CancelReservationIfNotPaidWeekBefore')
+EXEC dbo.sp_delete_job @job_name = N'CancelReservationIfNotPaidWeekBefore', @delete_unused_schedule = 1;
+GO
 EXEC dbo.sp_add_job
 	@job_name = N'CancelReservationIfNotPaidWeekBefore'
 GO
@@ -10,9 +13,11 @@ EXEC sp_add_jobstep
 	@subsystem = N'TSQL',
 	@command = N'EXEC dbo.CHECK_RESERVATIONS_FOR_CANCELLING';
 GO
+IF NOT EXISTS(SELECT * FROM msdb.dbo.sysschedules WHERE name = N'RunDaily')
 EXEC dbo.sp_add_schedule
 	@schedule_name = N'RunDaily',
 	@freq_type = 4,
+	@freq_interval = 1,
 	@active_start_time = 060000;
 GO
 EXEC sp_attach_schedule
